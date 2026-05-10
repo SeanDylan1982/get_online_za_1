14c12d86669f6aa3da349122ae741f68c97e604d
Sean Dylan Patterson <seandylanpatterson@gmail.com>
2026-05-10 20:07:26 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+      "motion-utils": path.resolve(__dirname, "node_modules/motion-utils/dist/cjs/index.js"),
+      "@floating-ui/core": path.resolve(__dirname, "node_modules/@floating-ui/core/dist/floating-ui.core.esm.js"),
+      "@floating-ui/dom": path.resolve(__dirname, "node_modules/@floating-ui/dom/dist/floating-ui.dom.esm.js"),
+      "@react-stately/utils": path.resolve(__dirname, "node_modules/@react-stately/utils/dist/main.js"),
+      "@react-aria/ssr": path.resolve(__dirname, "node_modules/@react-aria/ssr/dist/main.js"),
     },
   },
 });

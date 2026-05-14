53608b902334521922b0a2b574c976bf72c16c45
Sean Dylan Patterson <seandylanpatterson@gmail.com>
2026-05-14 19:57:24 +0200

    nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="bg-white shadow-md fixed top-0 left-0 w-full z-50">
-      <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center opacity-20">
+      <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
         <Link to="/" className="text-xl font-bold text-blue-600">
           MyBusinessOnline.co.za
         </Link>
